202d9d541cfb3c698b48d313e1a1869c2c4e61d6
POATAN <118012370+The0blessed@users.noreply.github.com>
2023-03-07 17:57:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function olamundo() {    
+  alert('Olá Mundo');}
+function calcularimc() 
+{    var kg, cm, imc   
+  kg = document.getElementById('kg').value    
+    cm = document.getElementById('cm').value    
+      imc = kg / cm  
+        alert("Seu IMC é:"+imc)}  
